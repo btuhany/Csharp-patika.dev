24afff893e769d034af200132cc09687306d2285
btuhany <114235184+btuhany@users.noreply.github.com>
2022-10-19 20:32:19 +0300

    Add files via upload

---
+++ odev1/Soru3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _vscode
+{
+    class Program
+    {
+        /*
+        Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+        Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+        Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+        
+        
+        */
+
+
+        static void Main(string[] args)
+        {
+            int n=0;
+
+            Console.WriteLine("Kac adet kelime girmek istiyorsunuz?");
+            
+            while(true)
+            {   
+                Console.Write("Lutfen pozitif bir sayi giriniz: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if(n>0)
+                    break;
+                else
+                    Console.WriteLine("Sifir veya negatif bir sayi girdiniz.");
+
+            }
+
+            string[] kelimeDizisi = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}.Kelimeyi giriniz: ",i+1);
+                kelimeDizisi[i] = Console.ReadLine();
+            }
+            for (int i = n-1; i >= 0; i--)
+            {
+                Console.WriteLine(kelimeDizisi[i]);
+            }
+
+        }
+    }
+}
---
+++ odev1/Soru4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Soru4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. 
+            Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+            */
+
+
+            
+            Console.Write("Lütfen cümlenizi giriniz:");
+            string cumle = Console.ReadLine();
+
+            
+            char[] harfDizisi= cumle.ToCharArray();
+            string[] kelimeDizisi=cumle.Split(" ");
+            
+            //bosluk karakterini harfDizisinden ayırmamız gerekiyor
+            //HARF SAYACI
+            int harfSayaci=0;
+            for (int i = 0; i < harfDizisi.Length; i++)
+            {
+                harfSayaci++;
+                if(harfDizisi[i]==' ')
+                    harfSayaci--;
+            }
+            Console.WriteLine("Girdiğiniz cümledeki harf sayisi: " +harfSayaci);
+            Console.WriteLine("Girdiğiniz cümledeki kelime sayisi: " +kelimeDizisi.Length);
+
+           
+        }
+    }
+}
