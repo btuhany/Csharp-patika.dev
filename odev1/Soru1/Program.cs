6cb4cb996c450cef9307178ad91fe302d6e1a787
btuhany <114235184+btuhany@users.noreply.github.com>
2022-10-19 19:47:57 +0300

    Add files via upload

---
+++ odev1/Soru1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            1)Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+            Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+            Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            */
+
+            int n=0;
+
+            Console.WriteLine("Kac adet sayi girmek istiyorsunuz?");
+            
+            while(true)
+            {   
+                Console.Write("Lutfen pozitif bir sayi giriniz: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if(n>0)
+                    break;
+                else
+                    Console.WriteLine("Sifir veya negatif bir sayi girdiniz.");
+
+            }
+            
+            int[] sayiDizisi = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}.Sayiyi giriniz: ",i+1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Girdiğiniz cift sayilar: ");
+            for (int i = 0; i < n; i++)
+            {
+                if(sayiDizisi[i]%2==0)
+                    Console.Write(sayiDizisi[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ odev1/Soru2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Soru2
+{
+    class Program
+    {
+
+        /*
+        Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
+        Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+        Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+        */
+        
+        static void Main(string[] args)
+        {
+
+   
+            int n=0;
+            int m=0;
+
+            while(true)
+            {   
+                Console.WriteLine("Lutfen pozitif iki adet sayi giriniz: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                m = Convert.ToInt32(Console.ReadLine());
+                if(n>0 && m>0)
+                    break;
+                else
+                    Console.WriteLine("Sifir veya negatif bir sayi girdiniz. Tekrar deneyiniz.");
+
+            }
+
+            int[] sayiDizisi = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0}.Sayiyi giriniz: ",i+1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if(sayiDizisi[i]%m==0)
+                    Console.Write(sayiDizisi[i] + " ");
+            }
+        }
+    }
+}
